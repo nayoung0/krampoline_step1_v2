1343db8793472c6b282ecb479b3f527b1358a643
nayoung <nayoung.tech@gmail.com>
2023-11-30 17:55:46 +0900

    Change port

--- index.py
+++ index.py
@@ -16,5 +16,38 @@ app = Flask(__name__)
 def get(): 
     return jsonify({"hello": "world!"})
 
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     payload = request.get_json()
+
+#     print('username', payload.get('username'))
+    
+#     print(payload)
+#     print(request.headers)
+    
+#     return jsonify({
+#         "id": 1,
+#         "start_at": "2023-12-25",
+#         "end_at": "2023-12-30",
+#         "username": "kidneybeans2"
+#     })
+
+# # @app.route('/users/<username>', methods=['POST'])
+# # def create_user(username):
+# #     payload = request.view_args.get('view_args')
+
+# #     print('username', payload.get('username'))
+    
+# #     print(payload)
+# #     print(request.headers)
+    
+# #     return jsonify({
+# #         "id": 1,
+# #         "start_at": "2023-12-25",
+# #         "end_at": "2023-12-30",
+# #         "username": "kidneybeans2"
+# #     })
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
