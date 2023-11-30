bf7e67ee07a3d30e72969f148c4db87b122c36c4
nayoung <nayoung.tech@gmail.com>
2023-12-01 05:42:18 +0900

    change response

--- index.py
+++ index.py
@@ -86,7 +86,7 @@ def create_map():
             "stamp_url": None
         },
         "lo_8": {
-            "created_at": None,
+            "created_at": "2023-12-20T00:00:00.000000+09:00",
             "memo": None,
             "stamp_url": None
         },
@@ -136,9 +136,9 @@ def get_map():
         "end_at": "2023-12-25T00:00:00.000000+09:00",
         "season": "winter",
         "lo_1": {
-            "created_at": "2023-12-25T00:00:00.000000+09:00",
-            "memo": "",
-            "stamp_url": ""
+            "created_at": None,
+            "memo": None,
+            "stamp_url": None
         },
         "lo_2": {
             "created_at": None,
@@ -151,29 +151,29 @@ def get_map():
             "stamp_url": None
         },
         "lo_4": {
-            "created_at": "2023-12-25T00:00:00.000000+09:00",
-            "memo": "",
-            "stamp_url": ""
-        },
-        "lo_5": {
-            "created_at": "2023-12-25T00:00:00.000000+09:00",
-            "memo": "",
-            "stamp_url": ""
-        },
-        "lo_6": {
-            "created_at": "2023-12-25T00:00:00.000000+09:00",
-            "memo": "",
-            "stamp_url": ""
-        },
-        "lo_7": {
-            "created_at": "2023-12-25T00:00:00.000000+09:00",
-            "memo": "",
-            "stamp_url": ""
-        },
-        "lo_8": {
             "created_at": None,
             "memo": None,
             "stamp_url": None
+        },
+        "lo_5": {
+            "created_at": None,
+            "memo": None,
+            "stamp_url": None
+        },
+        "lo_6": {
+            "created_at": None,
+            "memo": None,
+            "stamp_url": None
+        },
+        "lo_7": {
+            "created_at": None,
+            "memo": None,
+            "stamp_url": None
+        },
+        "lo_8": {
+            "created_at": "2023-12-24T00:00:00.000000+09:00",
+            "memo": "너무 예쁜 크리스마스 박물관! 딱 내 스타일이다 💖 평생 잊지 못할 크리스마스 ⛄️❄️",
+            "stamp_url": "https://drive.google.com/file/d/10pWHdJfClzg2Jfcx3mIDnLlmGWLATgFx/view?usp=drive_link"
         },
         "lo_9": {
             "created_at": None,
@@ -186,8 +186,8 @@ def get_map():
             "stamp_url": None
         },
         "lo_11": {
-            "created_at": None,
-            "memo": None,
+            "created_at": "2023-12-23T15:00:00.000000+09:00",
+            "memo": "오늘 제주도의 1100고지에서 보낸 시간은 정말 꿈만 같았다. 눈으로 뒤덮인 자연 경관과 평화로움이 내 마음을 완전히 사로잡았다. 정말 잊지 못할 경험이다!🍃✨❄️",
             "stamp_url": None
         },
         "lo_12": {
@@ -196,14 +196,14 @@ def get_map():
             "stamp_url": None
         },
         "lo_13": {
-            "created_at": None,
-            "memo": None,
-            "stamp_url": None
+            "created_at": "2023-12-22T10:00:00.000000+09:00",
+            "memo": "유채꽃이 하나도 안 펴있을까봐 한참 걱정했는데, 너무 예쁘게 피어있었다. 역시 제주도 여행은 겨울인가?🌼🌼",
+            "stamp_url": "https://drive.google.com/file/d/1iMUDe3A3BBh_W2A2QXi7zl1-dPouAYbt/view?usp=sharing"
         },
         "lo_14": {
-            "created_at": None,
-            "memo": None,
-            "stamp_url": None
+            "created_at": "2023-12-22T16:00:00.000000+09:00",
+            "memo": "내가 가 있을 때 어떻게 꽃이 활짝 피어 있었지? 🌺 이번 제주도 나의 여행은 진짜 운이 좋다!",
+            "stamp_url": "https://drive.google.com/drive/u/0/folders/1mfxrQ6ZxcdS2XnZmwWpiFr0MsMGY5gY7"
         },
         "lo_15": {
             "created_at": None,
